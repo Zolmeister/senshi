745c7f838b3d45d580698d4e7c26bd46e163ae20
Zoli Kahan <zolikahan@gmail.com>
2013-09-02 20:50:54 -0500

    added diff based updates

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ io.sockets.on('connection', function (socket) {
   var p;
 
   // on socket connect, start streaming game data to them
-  socket.emit('setItems', items)
+  socket.emit('highScores', highScores.slice(0, 10))
+  socket.emit('setState', arena)
 
   // socket join game (gives name), adds them to arena
   socket.on('join', function (name) {
@@ -35,8 +36,9 @@ io.sockets.on('connection', function (socket) {
 
       p = new Player(name, socket.id)
       arena.players.push(p)
+      diff[0].push(p)
+      // socket.emit('name', name)
       socket.emit('id', socket.id)
-      socket.emit('highScores', highScores.slice(0,10))
       return
     }
     socket.emit('taken', name)
@@ -49,8 +51,8 @@ io.sockets.on('connection', function (socket) {
 
   // on socket command (movement/attack), update game state
   socket.on('keydown', function (key) {
-    if(!p) return
-    
+    if (!p) return
+
     if (key == 32 || key == 90) return p.attacking = 1
     if (key > 36 && key < 41) {
 
@@ -62,7 +64,7 @@ io.sockets.on('connection', function (socket) {
     }
   })
   socket.on('keyup', function (key) {
-    if(!p) return
+    if (!p) return
     p.keys.splice(p.keys.indexOf(key), 1)
   })
 
@@ -74,55 +76,6 @@ io.sockets.on('connection', function (socket) {
 
 
 //game
-var arena = {
-  players: [],
-  bullets: []
-};
-var highScores = [];
-
-// stub 10 high scores
-for (var i = 0; i < 10; i++) {
-  highScores.push({
-    name: '',
-    score: ''
-  })
-}
-
-function Player(name, id) {
-  this.name = name
-  this.id = id
-  this.x = Math.floor(Math.random() * 300) + 50
-  this.y = Math.floor(Math.random() * 100) + 50
-  this.health = 100
-  this.kills = 0
-
-  // weapons: fists, machete, bow, gun - [-1, 0, 1, 2]
-  this.weapon = -1
-
-  // directions: left, up, right, down - 0, 1, 2, 3
-  this.dir = 3
-
-  // animation frame
-  this.frame = 0
-
-  this.keys = []
-  this.attacking = 0
-
-  while (collide(this, arena.players)) {
-    this.x = Math.floor(Math.random() * 300) + 50
-    this.y = Math.floor(Math.random() * 100) + 50
-  }
-}
-
-function Bullet(type, x, y, dir, shooter) {
-  // types: arrow, bullet - [0, 1]
-  this.type = type
-  this.shooter = shooter
-  this.x = x
-  this.y = y
-  this.dir = dir
-}
-
 var random = (function rng() {
   var x = 123456789,
     y = 362436069,
@@ -176,15 +129,83 @@ var items = (function generateMap() {
   return m
 })();
 
+var arena = {
+  players: [],
+  bullets: [],
+  items: items
+};
+var highScores = [];
+
+// stub 10 high scores
+for (var i = 0; i < 10; i++) {
+  highScores.push({
+    name: '',
+    score: ''
+  })
+}
+
+function Player(name, id) {
+  this.name = name
+  this.id = id
+  this.x = Math.floor(Math.random() * 300) + 50
+  this.y = Math.floor(Math.random() * 100) + 50
+  this.health = 100
+  this.kills = 0
+
+  // weapons: fists, machete, bow, gun - [-1, 0, 1, 2]
+  this.weapon = -1
+
+  // directions: left, up, right, down - 0, 1, 2, 3
+  this.dir = 3
+
+  // animation frame
+  this.frame = 1
+
+  this.keys = []
+  this.attacking = 0
+
+  while (collide(this, arena.players)) {
+    this.x = Math.floor(Math.random() * 300) + 50
+    this.y = Math.floor(Math.random() * 100) + 50
+  }
+}
+
+function Bullet(type, x, y, dir, shooter) {
+  // types: arrow, bullet - [0, 1]
+  this.type = type
+  this.shooter = shooter
+  this.x = x
+  this.y = y
+  this.dir = dir
+}
+
+// This diff will be sent and applied by the client to sync their arena
+// diff[0-2] = players
+// diff[0] = new players (append to end)
+// diff[1] = del players indicies (splice, starting in reverse order)
+// diff[2] = player updates (i:index, updated attrs)
+// diff[3-5] = bullets
+// diff[6-8] = items
+var diff = Array.apply([], new Array(9)).map(function () {
+  return []
+});
+
+function clone(o) {
+  return JSON.parse(JSON.stringify(o))
+}
+
 function physics(frame) {
   var players = arena.players
   var bullets = arena.bullets
+  var items = arena.items
+  var arenaClone = clone(arena)
+  
   // dir: [delta x, delta y]
   var keymap = {
-    0: [-1, 0], // left
-    1: [0, -1], // up
-    2: [1, 0], // right
-    3: [0, 1], // down
+    0: [-2, 0], // left
+    1: [0, -2], // up
+    2: [2, 0], // right
+    3: [0, 2], // down
   }
 
   // player movement
@@ -212,7 +233,10 @@ function physics(frame) {
               }
             }
           } else {
-            arena.bullets.push(new Bullet(player.weapon - 1, player.x, player.y, player.dir, player.id))
+            var bullet = new Bullet(player.weapon - 1, player.x, player.y, player.dir, player.id)
+            arena.bullets.push(bullet)
+            diff[3].push(bullet)
+            arenaClone.bullets.push(bullet)
           }
           player.frame++;
           player.attacking = (player.attacking + 1) % 5
@@ -221,21 +245,19 @@ function physics(frame) {
           player.attacking = (player.attacking + 1) % 5
         }
       }
-    } else {
-      if (keymap[key]) {
-        if (frame % 6 == 0) {
-          player.frame = (player.frame + 1) % 4
-        }
-        player.x += keymap[key][0]
-        player.y += keymap[key][1]
-        if (player.x < -400 || player.x > (400 - 16) || player.y < -300 || player.y > (300 - 18) || collide(player, players.slice(0, i).concat(players.slice(i + 1)))) {
-          player.x -= keymap[key][0]
-          player.y -= keymap[key][1]
-        }
-        player.dir = key
-      } else {
-        player.frame = 1
+    } else if (keymap[key]) {
+      if (frame % 6 == 0) {
+        player.frame = (player.frame + 1) % 4
       }
+      player.x += keymap[key][0]
+      player.y += keymap[key][1]
+      if (player.x < -400 || player.x > (400 - 16) || player.y < -300 || player.y > (300 - 18) || collide(player, players.slice(0, i).concat(players.slice(i + 1)))) {
+        player.x -= keymap[key][0]
+        player.y -= keymap[key][1]
+      }
+      player.dir = key;
+    } else {
+      player.frame = 1
     }
   }
 
@@ -257,12 +279,15 @@ function physics(frame) {
           }
         }
       }
+      
+      diff[4].push(i)
       bullets.splice(i, 1)
     } else if (bullet.x < -400 || bullet.x > 400 || bullet.y < -300 || bullet.y > 300) {
+      diff[4].push(i)
       bullets.splice(i, 1)
     }
   }
-  
+
   // player pickup items
   for (var i = items.length - 1; i >= 0; i--) {
     var item = items[i]
@@ -280,18 +305,11 @@ function physics(frame) {
 
           // drop current weapon
           item.id = player.weapon
-          io.sockets.emit('item', {
-            index: i,
-            val: item.id
-          })
         } else {
 
           // remove the item
           items.splice(i, 1)
-          io.sockets.emit('item', {
-            index: i,
-            del: 1
-          })
+          diff[7].push(i)
         }
 
         player.weapon = weapon
@@ -302,10 +320,6 @@ function physics(frame) {
   // player deaths
   for (var i = players.length - 1; i >= 0; i--) {
     var player = players[i]
-    if(!player){
-      console.log('undef?', player, i)
-      console.log(players)
-    }
     if (player.health <= 0) {
       // drop weapon
       if (player.weapon != -1) {
@@ -315,32 +329,84 @@ function physics(frame) {
           y: player.y
         }
         items.push(item)
-        io.sockets.emit('item', {
-          index: items.length - 1,
-          val: item
-        })
+        arenaClone.items.push(item)
+        diff[6].push(item)
       }
       // anounce death
       io.sockets.emit('alert', player.name + ' has been killed')
 
       // update high scores
       // TODO - only send if top 10 change
-      highScores.push({name: player.name, score: player.kills*1000})
-      
-      highScores.sort(function(a, b) {
-        return a.score - b.score
-      }).reverse()
-      
+      highScores.push({
+        name: player.name,
+        score: player.kills * 1000
+      })
+
+      highScores.sort(function (a, b) {
+        return -a.score + b.score
+      })
+
       // send high score list
-      io.sockets.emit('highScores', highScores.slice(0,10))
+      io.sockets.emit('highScores', highScores.slice(0, 10))
 
       players.splice(i, 1)
+      diff[1].push(i)
     }
   }
+
+  
+  // calculate update diffs
+  // apply death diffs to clone
+  // players
+  for(var i=0;i<diff[1].length;i++) {
+    arenaClone.players.splice(i,1)
+  }
+  var pDiffs = differ(players, arenaClone.players)
+  
+  // bullets
+  for(var i=0;i<diff[4].length;i++) {
+    arenaClone.bullets.splice(i,1)
+  }
+  var bDiffs = differ(bullets, arenaClone.bullets)
+  // items
+  for(var i=0;i<diff[7].length;i++) {
+    arenaClone.items.splice(i,1)
+  }
+  var iDiffs = differ(items, arenaClone.items)
+  
+  diff[2] = pDiffs
+  diff[5] = bDiffs
+  diff[8] = iDiffs
+  
+  var t = diff
+  diff = Array.apply([], new Array(9)).map(function () {
+    return []
+  })
+
+  return t
 }
+
+function differ(current, clone) {
+  var diffs = []
+  if(current.length != clone.length) {
+    console.log('cur', current, 'cl', clone)
+    throw new Error("AAAH")
+}
+  for (var i=0;i<current.length;i++) {
+    var update = {i:i}
+    var obj = current[i]
+    var cloned = clone[i]
+    for(var key in obj) {
+      if(key=='keys') continue
+      if(obj[key]!=cloned[key]) update[key] = obj[key]
+    }
+    if(Object.keys(update).length>1) diffs.push(update)
+  }
+  return diffs
+}
+
 var pHeight = 18
 var pWidth = 12;
-
 function collide(a, bs) {
   for (var i = 0; i < bs.length; i++) {
     var b = bs[i]
@@ -352,7 +418,14 @@ function collide(a, bs) {
 var frame = 0
 setInterval(function () {
   // update game state
-  physics(++frame)
+  var diff = physics(++frame)
+  
+  // don't send if empty
+  if(!diff.reduce(function(total, x) {
+    return total + x.length
+  }, 0)) return
+     
+  console.log(diff)
   // send game state data
-  io.sockets.emit('state', arena)
-}, 20)
+  io.sockets.emit('diffState', diff)
+}, 1000/30)
--- main.js
+++ main.js
@@ -26,24 +26,12 @@ socket.on('id', function (d) {
 // on death, draw instructios
 socket.on('dead', drawInstructions)
 // on game data, display on canvas
-socket.on('state', gameState)
+socket.on('setState', setState)
+socket.on('diffState', mergeDiff)
 // on item diff data, update item list
-socket.on('item', function (diff) {
-  console.log('got item diff', diff)
-  if (diff.del) {
-    items.splice(diff.index, 1)
-  } else {
-    items[diff.index] = diff.val
-  }
-})
 
-var items = []
 var alerts = []
-socket.on('setItems', function (serverItems) {
-  items = serverItems
-})
 socket.on('highScores', function (scores) {
-  console.log(scores)
   var $s = $('#scores')
   $s.innerHTML = '<h3>Top 10</h3>'
 
@@ -52,9 +40,10 @@ socket.on('highScores', function (scores) {
     var len = (scores[i].name + scores[i].score).length
     $s.innerHTML += '<h5>' + scores[i].name + repeat('&nbsp;', 22 - len) + scores[i].score + '</h5>'
   }
-})
+});
+
 function repeat(s, n) {
-  return new Array(n+1).join(s)
+  return new Array(n + 1).join(s)
 }
 socket.on('alert', function alert(msg) {
   // split alert string into many msgs
@@ -143,15 +132,13 @@ function draw() {
   ctx.clearRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height)
   var players = arena && arena.players
 
-  id = id || players && players[0] && players[0].id
-  if (!id) return
   var me;
-
   ctx.fillStyle = '#fff'
   for (var i = 0; i < players.length; i++) {
     var player = players[i]
     if (player.id == id) me = player
   }
+  me = me || players[0]
   if (!me) return
 
   //draw terrain
@@ -224,88 +211,149 @@ var map = (function generateMap() {
     m.push(temp)
   }
   return m
-})()
+})();
 
-  function drawTerrain(offsetX, offsetY) {
-    var row = 0
-    var col = 2
+function drawTerrain(offsetX, offsetY) {
+  var row = 0
+  var col = 2
 
-    for (var y = -canvas.height / 2; y < canvas.height / 2; y += 14) {
-      for (var x = -canvas.width / 2; x < canvas.width / 2; x += 14) {
-        if (x - offsetX + 14 < -canvas.width / 4 / 2 || y - offsetY + 14 < -canvas.height / 4 / 2 || x - offsetX > canvas.width / 4 / 2 || y - offsetY > canvas.height / 4 / 2) continue
-        ctx.drawImage(image, image.width - col * 14, map[(y + canvas.height / 2) / 14][(x + canvas.width / 2) / 14] * 14, 14, 14, x - offsetX, y - offsetY, 14, 14)
-      }
+  for (var y = -canvas.height / 2; y < canvas.height / 2; y += 14) {
+    for (var x = -canvas.width / 2; x < canvas.width / 2; x += 14) {
+      if (x - offsetX + 14 < -canvas.width / 4 / 2 || y - offsetY + 14 < -canvas.height / 4 / 2 || x - offsetX > canvas.width / 4 / 2 || y - offsetY > canvas.height / 4 / 2) continue
+      ctx.drawImage(image, image.width - col * 14, map[(y + canvas.height / 2) / 14][(x + canvas.width / 2) / 14] * 14, 14, 14, x - offsetX, y - offsetY, 14, 14)
     }
   }
+}
 
-  function drawItems(offsetX, offsetY) {
-    var row = 0
-    var col = 1
-    for (var i = 0; i < items.length; i++) {
-      var item = items[i]
-      if (item.x - offsetX + 14 < -canvas.width / 4 / 2 || item.y - offsetY + 14 < -canvas.height / 4 / 2 || item.x - offsetX > canvas.width / 4 / 2 || item.y - offsetY > canvas.height / 4 / 2) continue
-      ctx.drawImage(image, image.width - col * 14, item.id * 14, 14, 14, item.x - offsetX, item.y - offsetY, 14, 14)
+function drawItems(offsetX, offsetY) {
+  var items = arena.items
+  var row = 0
+  var col = 1
+  for (var i = 0; i < items.length; i++) {
+    var item = items[i]
+    if (item.x - offsetX + 14 < -canvas.width / 4 / 2 || item.y - offsetY + 14 < -canvas.height / 4 / 2 || item.x - offsetX > canvas.width / 4 / 2 || item.y - offsetY > canvas.height / 4 / 2) continue
+    ctx.drawImage(image, image.width - col * 14, item.id * 14, 14, 14, item.x - offsetX, item.y - offsetY, 14, 14)
+  }
+}
+
+function drawBullets(offsetX, offsetY) {
+  var bullets = arena.bullets
+
+  var row = 3
+  var col = 1
+  for (var i = 0; i < bullets.length; i++) {
+    var bullet = bullets[i]
+    if (bullet.x - offsetX + 14 < -canvas.width / 4 / 2 || bullet.y - offsetY + 14 < -canvas.height / 4 / 2 || bullet.x - offsetX > canvas.width / 4 / 2 || bullet.y - offsetY > canvas.height / 4 / 2) continue
+    if (bullet.type == 0) {
+      ctx.save()
+      ctx.translate(bullet.x - offsetX + 7, bullet.y - offsetY + 7)
+      // left, up, right, down - 0, 1, 2, 3
+      var rotate = [1, -.5, 0, .5]
+      ctx.rotate(rotate[bullet.dir] * Math.PI)
+      ctx.drawImage(image, image.width - col * 14, row * 14, 14, 14, -7, -7, 14, 14)
+      ctx.restore()
+    } else {
+      ctx.fillStyle = '#666'
+      ctx.fillRect(bullet.x - offsetX + (bullet.dir == 1 ? 10 : 5), bullet.y - offsetY + 7, bullet.dir % 2 == 0 ? 5 : 2, bullet.dir % 2 == 0 ? 2 : 5)
     }
   }
+}
 
-  function drawBullets(offsetX, offsetY) {
-    var bullets = arena.bullets
+function drawPlayer(x, y, name, health, dir, frame, weapon, kills) {
 
-    var row = 3
-    var col = 1
-    for (var i = 0; i < bullets.length; i++) {
-      var bullet = bullets[i]
-      if (bullet.x - offsetX + 14 < -canvas.width / 4 / 2 || bullet.y - offsetY + 14 < -canvas.height / 4 / 2 || bullet.x - offsetX > canvas.width / 4 / 2 || bullet.y - offsetY > canvas.height / 4 / 2) continue
-      if (bullet.type == 0) {
-        ctx.save()
-        ctx.translate(bullet.x - offsetX + 7, bullet.y - offsetY + 7)
-        // left, up, right, down - 0, 1, 2, 3
-        var rotate = [1, -.5, 0, .5]
-        ctx.rotate(rotate[bullet.dir] * Math.PI)
-        ctx.drawImage(image, image.width - col * 14, row * 14, 14, 14, -7, -7, 14, 14)
-        ctx.restore()
-      } else {
-        ctx.fillStyle = '#666'
-        ctx.fillRect(bullet.x - offsetX + (bullet.dir == 1 ? 10 : 5), bullet.y - offsetY + 7, bullet.dir % 2 == 0 ? 5 : 2, bullet.dir % 2 == 0 ? 2 : 5)
-      }
+  // 22 x 20, with +10 x-offset
+  var row = dir == 0 ? 1 : dir - 1
+  var col = frame == 3 ? 1 : frame
+  col += (weapon + 1) * 7
+  x += 10
+
+  // draw name
+  ctx.fillStyle = '#fff'
+  ctx.font = '3px sans'
+  ctx.fillText(name + ' (' + kills + ')', (x - name.length + 12), y - 2)
+
+  // draw health bar
+  ctx.fillStyle = '#3a3'
+  ctx.fillRect(x + 1, y - 1, health / 5, 1)
+  ctx.fillStyle = '#a33'
+  ctx.fillRect(x + 1 + health / 5, y - 1, 100 / 5 - health / 5, 1)
+
+  ctx.save()
+  if (dir == 0) {
+    ctx.translate(44, 0)
+    ctx.scale(-1, 1)
+    x = 22 - x
+  }
+  ctx.fillStyle = '#fff'
+  //ctx.fillRect(x,y,22,20)
+
+  //draw character
+  ctx.drawImage(image, col * 22, row * 20, 22, 20, x, y, 22, 20)
+  ctx.restore()
+
+}
+
+function setState(state) {
+  arena = state
+  draw()
+}
+
+// This diff will be sent and applied by the client to sync their arena
+// diff[0-2] = players
+// diff[0] = new players (append to end)
+// diff[1] = del players indicies (splice, starting in reverse order)
+// diff[2] = player updates (i:index, updated attrs)
+// diff[3-5] = bullets
+// diff[6-8] = items
+function mergeDiff(diff) {
+
+  // players
+  for(var i=0;i<diff[0].length;i++) {
+    arena.players.push(diff[0][i])
+  }
+  for(var i=0;i<diff[1].length;i++) {
+    arena.players.splice(diff[1][i], 1)
+  }
+  for(var i=0;i<diff[2].length;i++) {
+    var update = diff[2][i]
+    var index = update.i
+    delete update.i
+    for(var key in update) {
+      arena.players[index][key] = update[key]
     }
   }
-
-  function drawPlayer(x, y, name, health, dir, frame, weapon, kills) {
-
-    // 22 x 20, with +10 x-offset
-    var row = dir == 0 ? 1 : dir - 1
-    var col = frame == 3 ? 1 : frame
-    col += (weapon + 1) * 7
-    x += 10
-
-    // draw name
-    ctx.fillStyle = '#fff'
-    ctx.font = '3px sans'
-    ctx.fillText(name + ' (' + kills + ')', (x - name.length + 12), y - 2)
-
-    // draw health bar
-    ctx.fillStyle = '#3a3'
-    ctx.fillRect(x + 1, y - 1, health / 5, 1)
-    ctx.fillStyle = '#a33'
-    ctx.fillRect(x + 1 + health / 5, y - 1, 100 / 5 - health / 5, 1)
-
-    ctx.save()
-    if (dir == 0) {
-      ctx.translate(44, 0)
-      ctx.scale(-1, 1)
-      x = 22 - x
+  
+  // bullets
+  for(var i=0;i<diff[3].length;i++) {
+    arena.bullets.push(diff[3][i])
+  }
+  for(var i=0;i<diff[4].length;i++) {
+    arena.bullets.splice(diff[4][i], 1)
+  }
+  for(var i=0;i<diff[5].length;i++) {
+    var update = diff[5][i]
+    var index = update.i
+    delete update.i
+    for(var key in update) {
+      arena.bullets[index][key] = update[key]
     }
-    ctx.fillStyle = '#fff'
-    //ctx.fillRect(x,y,22,20)
-
-    //draw character
-    ctx.drawImage(image, col * 22, row * 20, 22, 20, x, y, 22, 20)
-    ctx.restore()
-
   }
-
-  function gameState(state) {
-    arena = state
-    draw()
+  
+  // items
+  for(var i=0;i<diff[6].length;i++) {
+    arena.items.push(diff[6][i])
   }
+  for(var i=0;i<diff[7].length;i++) {
+    arena.items.splice(diff[7][i])
+  }
+  for(var i=0;i<diff[8].length;i++) {
+    var update = diff[8][i]
+    var index = update.i
+    delete update.i
+    for(var key in update) {
+      arena.items[index][key] = update[key]
+    }
+  }
+  
+  draw()
+}
